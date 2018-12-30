e2baf280f3586cb0e440315751cafdce145d3bb0
Zac Finger <zac@Zacs-MacBook-Pro.local>
2018-12-30 13:54:09 -0700

    User registration passed QA

--- index.js
+++ index.js
@@ -125,7 +125,7 @@ app.post('/createUser', (req, res) => {
       username: req.body.username,
       password: req.body.password
     })
-    .then(() => res.sendStatus(200))
+    .then(() => res.sendStatus(200) )
 })
 app.post('/login', (req, res) => {
   store
